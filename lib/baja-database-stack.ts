4e38cdb83dc00d1ce8599edec32ee141d9c36fd0
Ben-Ferreira <benjamin@ferreira-Family.com>
2022-11-17 19:47:11 -0600

    TEST COMMIT

--- lib/baja-database-stack.ts
+++ lib/baja-database-stack.ts
@@ -1,10 +1,11 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BajaDatabaseStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    //TEST
 
     // The code that defines your stack goes here
 
